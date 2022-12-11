00fa864493a1ac89005ac7b039ff106e56537aff
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2022-12-12 01:21:34 +0700

    finishes layout for large viewport

--- js/main.js
+++ js/main.js
@@ -1,120 +1,131 @@
 
 $(document).ready(function() {
-
-    const showNavbarBackground = () => {
-        if ($("#largerNavbar").offset().top > 150) { // show after scroll to 150px from top
-            $("#largerNavbar").css({
-                background: "top right / 90% url('../img/purplish-gradient-lg.png') no-repeat, #0F1320",
-                opacity: "0.95",
-                backdropFilter: "blur(10px)",
-            })
-        } else {
-            $("#largerNavbar").css({
-                background: "none",
-                opacity: "1",
-                backdropFilter: "none",
-            })
+    const cb = () => {
+        
+        const showNavbarBackground = () => {
+            if ($("#largerNavbar").offset().top > 150) { // show after scroll to 150px from top
+                $("#largerNavbar").css({
+                    background: "top right / 90% url('../img/purplish-gradient-lg.png') no-repeat, #0F1320",
+                    opacity: "0.95",
+                    backdropFilter: "blur(10px)",
+                })
+            } else {
+                $("#largerNavbar").css({
+                    background: "none",
+                    opacity: "1",
+                    backdropFilter: "none",
+                })
+            }
         }
-    }
 
-    // call once for initial render
-    showNavbarBackground()
-
-    // call on scroll
-    $(document).on("scroll", () => { 
+        // call once for initial render
         showNavbarBackground()
-    })
 
-    // Open Side Menu
-    $(".open_hamburger_menu").on("click", () => {
-        $(".navbar_side_menu").toggleClass("d-none")
-        $(".navbar_side_menu").animate({ opacity: "1", zIndex: "999999" }, 400)
-    })
-    // Close Side Menu
-    $(".close_hamburger_menu").on("click", () => {
-        $(".navbar_side_menu").animate({ opacity: "0", zIndex: "-999999" }, 400)
-        setTimeout( $(".navbar_side_menu").toggleClass("d-none"), 400)
-    })
+        // call on scroll
+        $(document).on("scroll", () => { 
+            showNavbarBackground()
+        })
 
-    // Enabling Slick JS
-    $(".hero_carousel").slick({
-        autoplay: true,
-        speed: 500,
-        arrows: false,
-        fade: true,
-        // easing: "swing",
-        // rtl: true,
-        dots: true,
-        // appendDots: ".hero_carousel",
-        // draggable: true,
-        // adaptiveHeight: true,
-        pauseOnFocus: true,
-        pauseOnHover: false,
-        pauseOnDotsHover: false,
-        // infinite: false,
-    })    
-    $(".hero_gadget_carousel").slick({
-        autoplay: true,
-        speed: 500,
-        arrows: false,
-        // fade: true,
-        // easing: "swing",
-        // rtl: true,
-        dots: true,
-        appendDots: ".hero_carousel",
-        // centerMode: true,
-        // draggable: true,
-        pauseOnFocus: true,
-        pauseOnHover: false,
-        pauseOnDotsHover: false,
-        // infinite: false,
-        autoplay: false
-    })    
+        // Open Side Menu
+        $(".open_hamburger_menu").on("click", () => {
+            $(".navbar_side_menu").toggleClass("d-none")
+            $(".navbar_side_menu").animate({ opacity: "1", zIndex: "999999" }, 400)
+        })
+        // Close Side Menu
+        $(".close_hamburger_menu").on("click", () => {
+            $(".navbar_side_menu").animate({ opacity: "0", zIndex: "-999999" }, 400)
+            setTimeout( $(".navbar_side_menu").toggleClass("d-none"), 400)
+        })
+
+        // Enabling Slick JS
+        $(".hero_carousel").slick({
+            autoplay: true,
+            speed: 500,
+            arrows: false,
+            fade: true,
+            // easing: "swing",
+            // rtl: true,
+            dots: true,
+            // appendDots: ".hero_carousel",
+            // draggable: true,
+            // adaptiveHeight: true,
+            pauseOnFocus: true,
+            pauseOnHover: false,
+            pauseOnDotsHover: false,
+            // infinite: false,
+        })    
+        $(".hero_gadget_carousel").slick({
+            autoplay: true,
+            speed: 500,
+            arrows: false,
+            // fade: true,
+            // easing: "swing",
+            // rtl: true,
+            dots: true,
+            appendDots: ".hero_carousel",
+            // centerMode: true,
+            // draggable: true,
+            pauseOnFocus: true,
+            pauseOnHover: false,
+            pauseOnDotsHover: false,
+            // infinite: false,
+            autoplay: false
+        })    
+            
+    //     // Carousel Dots
         
-//     // Carousel Dots
-    
-//     if ($(".slick-dots li").hasClass("slick-active")) {
-//         $(".slick-dots li").html("<input type='radio' name='hero-carousel-dots' checked>")
-//     } else {
-//         $(".slick-dots li").html("<input type='radio' name='hero-carousel-dots'>")
-//     }
-    $(".slick-dots").addClass("my-4")
-    
-//     // $("#hero_intro").change(function(e) {
-//     //     console.log("changed")
-//     // })
-    
-
-    $(".slick-dots li").addClass("list-unstyled d-flex align-items-center")
-    $(".slick-dots li:nth-child(2)").addClass("px-2")
-    $(".slick-dots li").parent().addClass("d-flex px-0")
-    $(".slick-dots li").html(`
-        <span class="dot_hollow pt-1">&#9675;</span> <span class="dot_filled">&#9679;</span>
-    `)
-
-    
-    // Helper function to detect change in an element
-
-    const config = { attributes: true, childList: false, subtree: false };
-    const observer = new MutationObserver( (mutationList, observer) => { 
-        // callback here
-        if (mutationList[0].target.classList.contains("slick-active")) {
-            $(".hero_carousel .slick-dots li:first-child").trigger("click") //.click()
-        } else {
-            $(".hero_carousel .slick-dots li:nth-child(2)").trigger("click") //.click()
-        }
-    } );
-    
-    observer.observe(
-        $(".hero_carousel .slick-dots li")[0], 
-        { attributes: true, childList: false, subtree: false }
-    );
+    //     if ($(".slick-dots li").hasClass("slick-active")) {
+    //         $(".slick-dots li").html("<input type='radio' name='hero-carousel-dots' checked>")
+    //     } else {
+    //         $(".slick-dots li").html("<input type='radio' name='hero-carousel-dots'>")
+    //     }
+        $(".slick-dots").addClass("my-4")
         
-    // observer.disconnect()
+    //     // $("#hero_intro").change(function(e) {
+    //     //     console.log("changed")
+    //     // })
+        
 
-    // shift #presale_details' content to natural position
-    const presaleDetailsHeight = $("#presale_details").height()
-    $("#presale_details #presale_details_content").css("transform", `translateY(${presaleDetailsHeight/5}px)`)
-    $("#how_to_buy").css("margin-top", `${presaleDetailsHeight/5}px`)
+        $(".slick-dots li").addClass("list-unstyled d-flex align-items-center")
+        $(".slick-dots li:nth-child(2)").addClass("px-2")
+        $(".slick-dots li").parent().addClass("d-flex px-0")
+        $(".slick-dots li").html(`
+            <span class="dot_hollow pt-1">&#9675;</span> <span class="dot_filled">&#9679;</span>
+        `)
+
+        
+        // Helper function to detect change in an element
+
+        const config = { attributes: true, childList: false, subtree: false };
+        const observer = new MutationObserver( (mutationList, observer) => { 
+            // callback here
+            if (mutationList[0].target.classList.contains("slick-active")) {
+                $(".hero_carousel .slick-dots li:first-child").trigger("click") //.click()
+            } else {
+                $(".hero_carousel .slick-dots li:nth-child(2)").trigger("click") //.click()
+            }
+        } );
+        
+        observer.observe(
+            $(".hero_carousel .slick-dots li")[0], 
+            { attributes: true, childList: false, subtree: false }
+        );
+            
+        // observer.disconnect()
+
+        // shift #presale_details' content to natural position
+        const presaleDetailsHeight = $("#presale_details").height()
+        $("#presale_details #presale_details_content").css({
+            "transform": `translateY(${presaleDetailsHeight/5}px)`, // down 20%
+            "top": `${presaleDetailsHeight/5}px`
+        }) 
+        $("#presale_details").css({
+            "margin-bottom": `-${presaleDetailsHeight/5}px`,
+        })
+        $("#how_to_buy").css("margin-top", `${presaleDetailsHeight/5}px`)
+    }
+    cb() // call once
+    $(window).resize(function() {
+        cb() // call on resize window
+    })
 })
-
